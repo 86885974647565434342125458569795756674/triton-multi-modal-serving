cdeff687db12e63fa21aa7aa195aad265657e763
c <c>
2024-05-12 01:27:20 +0800

    tasks

---
+++ eevee_server.py
@@ -0,0 +1,177 @@
+import http.server
+import socketserver
+import queue
+import threading
+import time
+import numpy as np
+from PIL import Image
+import torch
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+import tritonclient.http as httpclient
+from tritonclient.utils import *
+from code.start_blip_vqa_tasks import blip_vqa_visual_encoder_task,blip_vqa_text_encoder_task,blip_vqa_text_decoder_task
+
+# Define a queue to store incoming POST requests
+request_queue = queue.Queue()
+
+# Define a dictionary to store processed results
+processed_results = {}
+
+# Define a dictionary to store threading Events for each request
+request_events = {}
+
+# Define a lock for accessing shared resources
+processed_results_lock = threading.Lock()
+request_events_lock = threading.Lock()
+
+# Define a class for handling HTTP requests
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(b"<html><body><h1>GET request received!</h1></body></html>")
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        request_id=time.time()
+
+        # Create an event for this request
+        request_event = threading.Event()
+        with request_events_lock:
+            request_events[request_id] = request_event
+
+        # Put the request in the queue
+        request_queue.put((request_id, post_data))
+
+        # Wait for the result to be available
+        request_event.wait()
+
+        # Get the result for this request
+        with processed_results_lock:
+            result = processed_results.get(request_id, b"Post data not available")
+
+        # Send the result back to the client
+        self._set_headers()
+        self.wfile.write(result)
+
+# Define a function to process the queue
+def blip_vqa_process_queue(batch_size_queue):
+    blip_vqa_visual_encoder_blip_vqa_text_encoder_queue=queue.Queue()
+    blip_vqa_text_encoder_blip_vqa_text_decoder_queue=queue.Queue()
+
+    blip_vqa_visual_encoder_batches_queue=queue.Queue()
+    blip_vqa_visual_encoder_task_thread=threading.Thread(target=blip_vqa_visual_encoder_task,args=(blip_vqa_visual_encoder_batches_queue,blip_vqa_visual_encoder_blip_vqa_text_encoder_queue))
+    blip_vqa_visual_encoder_task_thread.start()
+
+    blip_vqa_text_encoder_batches_queue=queue.Queue()
+    blip_vqa_text_encoder_task_thread=threading.Thread(target=blip_vqa_text_encoder_task,args=(blip_vqa_text_encoder_batches_queue,blip_vqa_visual_encoder_blip_vqa_text_encoder_queue,blip_vqa_text_encoder_blip_vqa_text_decoder_queue))
+    blip_vqa_text_encoder_task_thread.start()
+
+    blip_vqa_text_decoder_batches_queue=queue.Queue()
+    blip_vqa_text_decoder_batches_return_queue=queue.Queue()
+    blip_vqa_text_decoder_task_thread=threading.Thread(target=blip_vqa_text_decoder_task,args=(blip_vqa_text_decoder_batches_queue,blip_vqa_text_encoder_blip_vqa_text_decoder_queue,blip_vqa_text_decoder_batches_return_queue))
+    blip_vqa_text_decoder_task_thread.start()
+
+    blip_vqa_visual_encoder_batch_size,blip_vqa_text_encoder_batch_size,blip_vqa_text_decoder_batch_size=1,1,1
+    
+    while True:
+        try:
+            batch_sizes=batch_size_queue.get(block=False)
+            if batch_sizes is None:
+                break
+            blip_vqa_visual_encoder_batch_size,blip_vqa_text_encoder_batch_size,blip_vqa_text_decoder_batch_size=batch_sizes[0],batch_sizes[1],batch_sizes[2]
+        except queue.Empty:
+            pass
+        if not request_queue.empty():
+            request_ids, batch_nums,images, texts=[],[],[],[]
+            while not request_queue.empty():
+                (request_id,image,text) = request_queue.get()
+                request_ids.append(request_id)
+                images.append(image)
+                texts.append(text)
+                batch_nums.append(image.shape[0])
+            
+            # cache remove image replication
+             
+            images=np.concatenate(images, axis=0)
+            num_blip_vqa_visual_encoder_batch=images.shape[0]//blip_vqa_visual_encoder_batch_size
+            num_blip_vqa_visual_encoder_left=images.shape[0]-num_blip_vqa_visual_encoder_batch*blip_vqa_visual_encoder_batch_size
+            if num_blip_vqa_visual_encoder_left!=0:
+                num_blip_vqa_visual_encoder_batch+=1
+                blip_vqa_visual_encoder_batches=np.split(images[:-num_blip_vqa_visual_encoder_left],blip_vqa_visual_encoder_batch_size)+[images[-num_blip_vqa_visual_encoder_left:]]
+            else:
+                blip_vqa_visual_encoder_batches=np.split(images,blip_vqa_visual_encoder_batch_size)
+            
+            texts=np.concatenate(texts, axis=0)
+            num_blip_vqa_text_encoder_batch=texts.shape[0]//blip_vqa_text_encoder_batch_size
+            num_blip_vqa_text_encoder_left=texts.shape[0]-num_blip_vqa_text_encoder_batch*blip_vqa_text_encoder_batch_size
+            if num_blip_vqa_text_encoder_left!=0:
+                num_blip_vqa_text_encoder_batch+=1
+                blip_vqa_text_encoder_batches=np.split(texts[:-num_blip_vqa_text_encoder_left],blip_vqa_text_encoder_batch_size)+[images[-num_blip_vqa_text_encoder_left:]]
+            else:
+                blip_vqa_text_encoder_batches=np.split(texts,blip_vqa_text_encoder_batch_size)
+
+            num_blip_vqa_text_decoder_batch=texts.shape[0]//blip_vqa_text_decoder_batch_size
+            num_blip_vqa_text_decoder_left=texts.shape[0]-num_blip_vqa_text_decoder_batch*blip_vqa_text_decoder_batch_size
+            if num_blip_vqa_text_decoder_left!=0:
+                num_blip_vqa_text_decoder_batch+=1
+                blip_vqa_text_decoder_batches=np.split(texts[:-num_blip_vqa_text_decoder_left],blip_vqa_text_decoder_batch_size)+[images[-num_blip_vqa_text_decoder_left:]]
+            else:
+                blip_vqa_text_decoder_batches=np.split(texts,blip_vqa_text_decoder_batch_size)
+
+            blip_vqa_visual_encoder_batches_queue.put(blip_vqa_visual_encoder_batches,block=False)
+            blip_vqa_text_encoder_batches_queue.put(blip_vqa_text_encoder_batches,block=False)
+            blip_vqa_text_decoder_batches_queue.put(blip_vqa_text_decoder_batches,block=False)
+
+            batch_count=0
+            now_left=None
+            for _ in range(num_blip_vqa_text_decoder_batch):
+                blip_vqa_text_decoder_batches_return=blip_vqa_text_decoder_batches_return_queue.get()
+                if now_left is None:
+                    now_left=blip_vqa_text_decoder_batches_return
+                else:
+                    now_left=np.concatenate([now_left,blip_vqa_text_decoder_batches_return],axis=0)
+                if now_left.shape[0]>=batch_nums[batch_count]:
+                    post_return=blip_vqa_text_decoder_batches_return[:batch_nums[batch_count]]
+                    now_left=now_left[batch_nums[batch_count]:]
+                else:
+                    continue
+                with processed_results_lock:
+                        processed_results[request_ids[batch_count]] = post_return
+                with request_events_lock:
+                    request_event = request_events.get(request_ids[batch_count])
+                request_event.set()
+                batch_count+=1
+    
+    blip_vqa_visual_encoder_batches_queue.put(None,block=False)
+    blip_vqa_visual_encoder_task_thread.join()
+
+    blip_vqa_text_encoder_batches_queue.put(None,block=False)
+    blip_vqa_text_encoder_task_thread.join()
+
+    blip_vqa_text_decoder_batches_queue.put(None,block=False)
+    blip_vqa_text_decoder_task_thread.join()
+
+
+if __name__=="__main__":
+    batch_size_queue=queue.Queue()
+    batch_size_queue.put((3,3,3),block=False)
+    blip_vqa_process_thread = threading.Thread(target=blip_vqa_process_queue,args=(batch_size_queue))
+    blip_vqa_process_thread.start()
+
+    with socketserver.TCPServer(("", 8000), RequestHandler) as httpd:
+        print("Server started at port 8000")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nShutting down server...")
+            httpd.shutdown()
+
+    batch_size_queue.put(None,block=False)
+    blip_vqa_process_thread.join()
--- start_blip_vqa_clients.py
+++ start_blip_vqa_clients.py
@@ -7,18 +7,47 @@ import tritonclient.http as httpclient
 from tritonclient.utils import *
 import time
 import threading
+import queue
 
-stop_processing=False
-blip_vqa_visual_encoder_batch_size=3
-blip_vqa_text_encoder_batch_size=3
-blip_vqa_text_decoder_batch_size=3
-
-def while_blip_vqa_visual_encoder():
-    global stop_processing,blip_vqa_visual_encoder_batch_size
-    model_name = "blip_vqa_visual_encoder"
+def while_client(model_name,input_queue,output_queue):
     with httpclient.InferenceServerClient("localhost:8000") as client:
-        while not stop_processing:
-            input0_data=np.array([b"/workspace/demos/images/merlion.png"]*blip_vqa_visual_encoder_batch_size)
+        while True:
+            input_data=input_queue.get()
+            if input_data is None:
+                break
+            inputs=[]
+            if isinstance(input_data,tuple):
+                for i in range(len(input_data)):
+                    inputs.append(
+                        httpclient.InferInput(
+                        f"INPUT{i}", input_data[i].shape, np_to_triton_dtype(input_data[i].dtype)
+                        )
+                    )
+                for i in range(len(inputs)):
+                    inputs[i].set_data_from_numpy(input_data[i])
+            else:
+                inputs.append(
+                    httpclient.InferInput(
+                    f"INPUT0", input_data.shape, np_to_triton_dtype(input_data.dtype)
+                    )
+                )
+                inputs[0].set_data_from_numpy(input_data)
+
+            outputs = [
+                httpclient.InferRequestedOutput("OUTPUT0"),
+            ]
+
+            response = client.infer(model_name, inputs, request_id=str(1), outputs=outputs)
+
+            output0_data = response.as_numpy("OUTPUT0")
+            output_queue.put(output0_data,block=False)
+
+def while_blip_vqa_visual_encoder(input_queue,output_queue):
+    with httpclient.InferenceServerClient("localhost:8000") as client:
+        while True:
+            input0_data=input_queue.get()
+            if input0_data is None:
+                break
             inputs = [
                 httpclient.InferInput(
                     "INPUT0", input0_data.shape, np_to_triton_dtype(input0_data.dtype)
@@ -31,24 +60,19 @@ def while_blip_vqa_visual_encoder():
                 httpclient.InferRequestedOutput("OUTPUT0"),
             ]
 
-            response = client.infer(model_name, inputs, request_id=str(1), outputs=outputs)
-
-            result = response.get_response()
+            response = client.infer("blip_vqa_visual_encoder", inputs, request_id=str(1), outputs=outputs)
 
             output0_data = response.as_numpy("OUTPUT0")
+            output_queue.put(output0_data,block=False)
 
-            print("INPUT0 ({})  = OUTPUT0 ({})".format(input0_data.shape, output0_data.shape))
 
-
-def while_blip_vqa_text_encoder():
-    global stop_processing,blip_vqa_text_encoder_batch_size
-    model_name = "blip_vqa_text_encoder"
+def while_blip_vqa_text_encoder(input_queue,output_queue):
     with httpclient.InferenceServerClient("localhost:8000") as client:
-        while not stop_processing:
-            input0_data = np.repeat(np.load("/workspace/pretrained/images_embeds.npy"),blip_vqa_text_encoder_batch_size,axis=0)
-            input1_data = np.array(
-                [b"where is the woman sitting?"]*blip_vqa_text_encoder_batch_size
-            )
+        while True:
+            input_data=input_queue.get()
+            if input_data is None:
+                break
+            input0_data,input1_data=input_data[0],input_data[1]
 
             inputs = [
                 httpclient.InferInput(
@@ -70,25 +94,17 @@ def while_blip_vqa_text_encoder():
                 "blip_vqa_text_encoder", inputs, request_id=str(1), outputs=outputs
             )
 
-            result = response.get_response()
-
             output0_data = response.as_numpy("OUTPUT0")
-
-            print(
-                "INPUT0 {} + INPUT1 {} = OUTPUT0 {}".format(
-                    input0_data.shape, input1_data, output0_data.shape
-                )
-            )
+            output_queue.put(output0_data,block=False)
 
 
 
-def while_blip_vqa_text_decoder():
-    global stop_processing,blip_vqa_text_decoder_batch_size
-    model_name = "blip_vqa_text_decoder"
+def while_blip_vqa_text_decoder(input_queue,output_queue):
     with httpclient.InferenceServerClient("localhost:8000") as client:
-        while not stop_processing:
-            input0_data = np.repeat(np.load("/workspace/pretrained/questions_states.npy"),blip_vqa_text_decoder_batch_size,axis=0)
-
+        while True:
+            input0_data = input_queue.get()
+            if input0_data is None:
+                break
             inputs = [
                 httpclient.InferInput(
                     "INPUT0", input0_data.shape, np_to_triton_dtype(input0_data.dtype)
@@ -105,40 +121,68 @@ def while_blip_vqa_text_decoder():
                 "blip_vqa_text_decoder", inputs, request_id=str(1), outputs=outputs
             )
 
-            result = response.get_response()
-
             output0_data = response.as_numpy("OUTPUT0")
-
-            print("INPUT0 {} = OUTPUT0 {}".format(input0_data.shape, output0_data))
+            output_queue.put(output0_data,block=False)
 
 
 if __name__=="__main__":
-    stop_processing=False
-    blip_vqa_visual_encoder_thread = threading.Thread(target=while_blip_vqa_visual_encoder,)
+    blip_vqa_visual_encoder_input_queue=queue.Queue()
+    blip_vqa_visual_encoder_output_queue=queue.Queue()
+    # blip_vqa_visual_encoder_thread = threading.Thread(target=while_blip_vqa_visual_encoder,args=(blip_vqa_visual_encoder_input_queue,blip_vqa_visual_encoder_output_queue))
+    # blip_vqa_visual_encoder_thread.start()
+    blip_vqa_visual_encoder_thread = threading.Thread(target=while_client,args=("blip_vqa_visual_encoder",blip_vqa_visual_encoder_input_queue,blip_vqa_visual_encoder_output_queue))
     blip_vqa_visual_encoder_thread.start()
-    time.sleep(5)
-    blip_vqa_visual_encoder_batch_size=2
-    time.sleep(5)
-    stop_processing=True
-    time.sleep(5)
-    blip_vqa_visual_encoder_thread.join()
+    for batch_size in [2,4]:
+        for _ in range(3):
+            input0_data=np.array([b"/workspace/demos/images/merlion.png"]*batch_size)
+            blip_vqa_visual_encoder_input_queue.put(input0_data,block=False)
+    for batch_size in [2,4]:
+        for _ in range(3):
+            input0_data=np.array([b"/workspace/demos/images/merlion.png"]*batch_size)
+            output0_data=blip_vqa_visual_encoder_output_queue.get()
+            print("INPUT0 ({})  = OUTPUT0 ({})".format(input0_data.shape, output0_data.shape))
 
-    stop_processing=False
-    blip_vqa_text_encoder_thread = threading.Thread(target=while_blip_vqa_text_encoder,)
+    blip_vqa_text_encoder_input_queue=queue.Queue()
+    blip_vqa_text_encoder_output_queue=queue.Queue()
+    # blip_vqa_text_encoder_thread = threading.Thread(target=while_blip_vqa_text_encoder,args=(blip_vqa_text_encoder_input_queue,blip_vqa_text_encoder_output_queue))
+    # blip_vqa_text_encoder_thread.start()
+    blip_vqa_text_encoder_thread = threading.Thread(target=while_client,args=("blip_vqa_text_encoder",blip_vqa_text_encoder_input_queue,blip_vqa_text_encoder_output_queue))
     blip_vqa_text_encoder_thread.start()
-    time.sleep(5)
-    blip_vqa_text_encoder_batch_size=2
-    time.sleep(5)
-    stop_processing=True
-    time.sleep(5)
-    blip_vqa_text_encoder_thread.join()
-
-    stop_processing=False
-    blip_vqa_text_decoder_thread = threading.Thread(target=while_blip_vqa_text_decoder,)
+    for batch_size in [2,4]:
+        for _ in range(3):
+            input0_data = np.repeat(np.load("/workspace/pretrained/images_embeds.npy"),batch_size,axis=0)
+            input1_data = np.array([b"where is the woman sitting?"]*batch_size)
+            blip_vqa_text_encoder_input_queue.put((input0_data,input1_data),block=False)
+    for batch_size in [2,4]:
+        for _ in range(3):
+            input0_data = np.repeat(np.load("/workspace/pretrained/images_embeds.npy"),batch_size,axis=0)
+            input1_data = np.array([b"where is the woman sitting?"]*batch_size)
+            output0_data=blip_vqa_text_encoder_output_queue.get()
+            print(
+                "INPUT0 {} + INPUT1 {} = OUTPUT0 {}".format(
+                    input0_data.shape, input1_data, output0_data.shape
+                )
+            )
+    
+    blip_vqa_text_decoder_input_queue=queue.Queue()
+    blip_vqa_text_decoder_output_queue=queue.Queue()
+    # blip_vqa_text_decoder_thread = threading.Thread(target=while_blip_vqa_text_decoder,args=(blip_vqa_text_decoder_input_queue,blip_vqa_text_decoder_output_queue))
+    # blip_vqa_text_decoder_thread.start()
+    blip_vqa_text_decoder_thread = threading.Thread(target=while_client,args=("blip_vqa_text_decoder",blip_vqa_text_decoder_input_queue,blip_vqa_text_decoder_output_queue))
     blip_vqa_text_decoder_thread.start()
-    time.sleep(5)
-    blip_vqa_text_decoder_batch_size=2
-    time.sleep(5)
-    stop_processing=True
-    time.sleep(5)
+    for batch_size in [2,4]:
+        for _ in range(3):
+            input0_data = np.repeat(np.load("/workspace/pretrained/questions_states.npy"),batch_size,axis=0)
+            blip_vqa_text_decoder_input_queue.put(input0_data,block=False)
+    for batch_size in [2,4]:
+        for _ in range(3):
+            input0_data = np.repeat(np.load("/workspace/pretrained/questions_states.npy"),batch_size,axis=0)
+            output0_data=blip_vqa_text_decoder_output_queue.get()
+            print("INPUT0 {} = OUTPUT0 {}".format(input0_data.shape, output0_data))
+
+    blip_vqa_visual_encoder_input_queue.put(None,block=False)
+    blip_vqa_visual_encoder_thread.join()
+    blip_vqa_text_encoder_input_queue.put(None,block=False)
+    blip_vqa_text_encoder_thread.join()
+    blip_vqa_text_decoder_input_queue.put(None,block=False)
     blip_vqa_text_decoder_thread.join()
